a5b012d40b5730e7d9369537e18fe9ee5dcb88bc
Vanessa Emily <vanemy@MacBook-Pro-de-Vanessa.local>
2026-08-17 16:27:13 -0600

    adding more explanation of logarithms

--- Python/happy_number.py
+++ Python/happy_number.py
@@ -23,6 +23,8 @@ class Solution(object):
 
 ## La complejidad espacial y temporal de este algoritmo es O(log n).
 ## porque recorre los digitos de n, la cantidad de digitos de un entero crece logaritmicamente con respecto a su valor de n
+# Incluso cuando son los digitos, el numero máximo del digito es 9, incluso teniendo 100 digitos, si el número máximo de d es 9, y el doble de 9 es 81
+# tendriamos 100 * 81 = 8,100, que es un número de 5 digitos que incluso disminuye considerablemente el tamaño, por lo que es más rápido que encontremos 1 o entremos en un ciclo.
 
 print(Solution().isHappy(19))  # Output: True
     
